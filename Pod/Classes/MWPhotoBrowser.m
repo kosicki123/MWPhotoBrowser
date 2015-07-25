ad4fdb3d61ac064a35ac03121dcc7a1be5626a7f
Renan Kosicki <renankosicki@gmail.com>
2015-07-25 13:52:19 -0300

    Exposed actionButton

--- Pod/Classes/MWPhotoBrowser.m
+++ Pod/Classes/MWPhotoBrowser.m
@@ -176,7 +176,12 @@ static void * MWVideoPlayerObservation = &MWVideoPlayerObservation;
         _nextButton = [[UIBarButtonItem alloc] initWithImage:nextButtonImage style:UIBarButtonItemStylePlain target:self action:@selector(gotoNextPage)];
     }
     if (self.displayActionButton) {
-        _actionButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(actionButtonPressed:)];
+        if (!self.actionButton) {
+            _actionButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(actionButtonPressed:)];
+        } else {
+            [self.actionButton setTarget:self];
+            [self.actionButton setAction:@selector(actionButtonPressed:)];
+        }
     }
     
     // Update
